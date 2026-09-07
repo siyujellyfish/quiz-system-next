0098931d001180434e20df126d10b951d152e790
siyujellyfish <siyujullyfish@gmail.com>
2026-09-07 13:59:58 +0800

    Use a fixed 64px header boundary

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,11 +17,11 @@
 </script>
 
 <div class="flex min-h-dvh flex-col">
-	<div class="fixed inset-x-0 top-0 z-[60]">
+	<div class="app-header-shell fixed inset-x-0 top-0 z-[60] h-16 overflow-visible">
 		<AppHeader user={data.user ?? null} />
 	</div>
 
-	<main class="app-page-shell flex min-w-0 flex-1 flex-col pt-10">
+	<main class="app-page-shell flex min-w-0 flex-1 flex-col pt-16">
 		{@render children()}
 	</main>
 </div>
